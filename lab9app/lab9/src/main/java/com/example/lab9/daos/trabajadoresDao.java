19e8a4197e5d22995ca09d2000e3a12c57b41b3e
Diego Henriquez <a20182044@pucp.edu.pe>
2023-10-23 20:25:07 -0500

    añadiendo cambios

--- lab9app/lab9/src/main/java/com/example/lab9/daos/trabajadoresDao.java
+++ lab9app/lab9/src/main/java/com/example/lab9/daos/trabajadoresDao.java
@@ -1,4 +1,45 @@
 package com.example.lab9.daos;
 
+import java.sql.*;
+import java.util.ArrayList;
+
+import com.example.lab9.beans.trabajadores;
+
 public class trabajadoresDao {
+    public ArrayList<trabajadores> listar(){
+        ArrayList<trabajadores> lista = new ArrayList<>();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/bicicentro";
+        String username = "root";
+        String password = "root";
+
+        String sql = "select * from trabajadores";
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                trabajadores trabajadores = new trabajadores();
+                trabajadores.setNombres(rs.getString(1));
+                trabajadores.setApellidos(rs.getString(2));
+                trabajadores.setCorreo(rs.getString(3));
+                trabajadores.setDni(rs.getString(4));
+
+                lista.add(trabajadores);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return lista;
+    }
+
+
 }
