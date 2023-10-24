f13f62f15169a3b38d352618fb1a406dc40f53c9
Diego Henriquez <a20182044@pucp.edu.pe>
2023-10-23 20:31:12 -0500

    cambios

--- lab9app/lab9/src/main/java/com/example/lab9/servlets/biciServlet.java
+++ lab9app/lab9/src/main/java/com/example/lab9/servlets/biciServlet.java
@@ -1,20 +1,38 @@
 package com.example.lab9.servlets;
 
+import com.example.lab9.beans.trabajadores;
+import com.example.lab9.daos.trabajadoresDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "biciServlet", value = "/biciServlet")
 public class biciServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
 
+        trabajadoresDao trabajadoresDao = new trabajadoresDao();
+
+        switch (action){
+            case "lista":
+                //saca del modelo
+                ArrayList<trabajadores> list = trabajadoresDao.listar();
+
+                //mandar la lista a la vista -> job/lista.jsp
+                request.setAttribute("lista",list);
+                RequestDispatcher rd = request.getRequestDispatcher("bicis/lista.jsp");
+                rd.forward(request,response);
+                break;
+        }
     }
 
-    @Override
+    /*@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+    }*/
 }
